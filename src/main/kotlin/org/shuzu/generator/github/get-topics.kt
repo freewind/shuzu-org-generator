38adbd8e338ab2156e356e489ef4604f2e531097
freewind <nowindlee@gmail.com>
2018-08-28 22:24:09 +0800

    fix topics

--- src/main/kotlin/org/shuzu/generator/github/get-topics.kt
+++ src/main/kotlin/org/shuzu/generator/github/get-topics.kt
@@ -2,13 +2,14 @@ package org.shuzu.generator.github
 
 import com.github.kittinunf.fuel.Fuel
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+
+data class Topics(val names: List<String>)
 
 fun getTopics(orgName: String, repoName: String): List<String>? {
     val response = Fuel.get("https://api.github.com/repos/$orgName/$repoName/topics")
             .header(mapOf("Accept" to "application/vnd.github.mercy-preview+json"))
             .responseString().third.get()
-    val listType = (object : TypeToken<List<String>>() {}).type
-    val topics = Gson().fromJson<List<String>?>(response, listType)
-    return topics?.takeIf { it.isNotEmpty() }
+    println("- topics: $response")
+    val topics = Gson().fromJson<Topics>(response, Topics::class.java)
+    return topics.names.takeIf { it.isNotEmpty() }
 }
